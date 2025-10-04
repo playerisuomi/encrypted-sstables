544345344cf4dcfa7c94c11df0eab7825493c080
kerosiinikone <onni.ahopelto@gmail.com>
2025-10-04 15:34:24 +0300

    Loading and parsing the latest segment to find a key

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Notes
 
-- SSTables -> background thread(pool) to merge
+- SSTables -> background thread(pool) to merge?
 - Main loop (on main thread) that listens for commands -> performs writes / reads with locks (`Arc<Mutex>`)
 - Background thread handles compaction and flushing (job queue)
   - Sleeps (_Condvar_) and listens for signals (_mpsc_)
@@ -10,9 +10,8 @@
 ### Next
 
 - _Merge segments_
-- Scan the segments efficiently on a read-through (key not in memory)
+- Scan the segments efficiently on a read-through (key not in memory) (1/2)
 - KvStore methods separated -> clean up
-- Serializing into an on-disk structure -> bytes?
 - Error propagation with `Results`
   - KvError
 - _More effecive way of getting the latest segment?_
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use serde::{Serialize, de::DeserializeOwned};
+use std::io::Seek;
 use std::{
     collections::BTreeMap,
     env::{self},
     fs::{self, File, OpenOptions, ReadDir},
     io::{Read, Write, stdin},
-    ops::DerefMut,
+    ops::{Add, DerefMut},
+    os::windows::fs::FileExt,
     sync::{Arc, Mutex, mpsc},
     thread::{self},
+    vec,
 };
 
 // Keep short for testing
 const MAX_MEMTABLE: usize = 1 << 2;
-const FOOTER_SIZE: usize = 1 << 2;
+const FOOTER_SIZE: usize = 1 << 4;
 
 struct KvStore<K, V> {
     memtable: BTreeMap<K, V>,
@@ -72,9 +75,11 @@ fn main() {
 
     let curr_dir_bg = curr_dir.clone();
     let log_handle_bg = log_handle.clone();
+    let seq_bg = seq_num.clone();
+
     let _ = thread::spawn(move || {
         for flush_table in rx {
-            let mut seq_num = seq_num.lock().unwrap().to_owned();
+            let mut seq_num = seq_bg.lock().unwrap();
             let mut seg_handle = OpenOptions::new()
                 .write(true)
                 .create(true)
@@ -90,31 +95,39 @@ fn main() {
             let mut buf = Vec::new();
 
             for (i, (k, v)) in flush_table.iter().enumerate() {
-                let (key_len, value_len) = (k.len(), v.len());
+                let offset = buf.len() as u64; // could be u32
+
+                let (key_len, value_len): (u32, u32) =
+                    (k.as_bytes().len() as u32, v.as_bytes().len() as u32);
+                // zero-bytes to indicate a key len in the byte stream?
                 buf.extend_from_slice(&key_len.to_be_bytes());
                 buf.extend_from_slice(k.as_bytes());
                 buf.extend_from_slice(&value_len.to_be_bytes());
                 buf.extend_from_slice(v.as_bytes());
 
-                if i % (flush_table.len() / 4) == 0 {
+                if i % (flush_table.len() / 2) == 0 {
                     // Sparse index
+                    println!("Put into index: {offset}");
                     idx.extend_from_slice(&key_len.to_be_bytes());
                     idx.extend_from_slice(k.as_bytes());
-                    idx.extend_from_slice(&value_len.to_be_bytes());
-                    idx.extend_from_slice(v.as_bytes());
-                    idx.extend_from_slice(&i.to_be_bytes());
+                    idx.extend_from_slice(&offset.to_be_bytes());
                 }
             }
-            let mut footer: Vec<u8> = vec![0; 4];
-            footer[0..4].copy_from_slice(&idx.len().to_be_bytes());
-            
+            let mut footer: Vec<u8> = vec![0; FOOTER_SIZE];
+            // offset
+            footer[0..FOOTER_SIZE / 2].copy_from_slice(&buf.len().to_be_bytes());
+            // size
+            footer[FOOTER_SIZE / 2..FOOTER_SIZE].copy_from_slice(&idx.len().to_be_bytes());
+
             buf.extend(&idx);
             buf.extend_from_slice(&footer);
+
+            // println!("Idx len: {}, footer len: {}", idx.len(), footer.len());
 
             seg_handle
                 .write_all(buf.as_slice())
                 .expect("Unable to write");
-            seq_num += 1;
+            *seq_num = seq_num.add(1);
 
             let mut log = log_handle_bg.lock().unwrap();
 
@@ -146,8 +159,6 @@ fn main() {
                 let (k, v) = (cmd_seq[1], cmd_seq[2]);
                 let (key_len, value_len) = (k.len(), v.len());
 
-                // Lengths as bytes -> reading?
-
                 assert!(key_len <= u8::MAX as usize);
                 assert!(value_len <= u8::MAX as usize);
 
@@ -176,7 +187,110 @@ fn main() {
                     println!("GET -> {}", val.clone())
                 } else {
                     // Assume no merging / compaction in the beginning!
-                    todo!()
+                    let curr_seq = seq_num.lock().unwrap().clone() - 1;
+                    // while found_value.is_none() {
+                    // Load the corresponding segment file
+                    let mut seg_file = File::open(
+                        curr_dir
+                            .join(format!("segment_{}.sstable", curr_seq))
+                            .as_path(),
+                    )
+                    .unwrap();
+                    // Read the footer
+                    let seg_size = seg_file.metadata().unwrap().len();
+
+                    let mut footer_bytes: [u8; FOOTER_SIZE] = [0; FOOTER_SIZE];
+                    let footer_offset = (seg_size as usize - FOOTER_SIZE) as u64;
+                    seg_file
+                        .seek_read(&mut footer_bytes[0..FOOTER_SIZE], footer_offset)
+                        .unwrap();
+
+                    let idx_offset =
+                        u64::from_be_bytes(footer_bytes[0..FOOTER_SIZE / 2].try_into().unwrap());
+                    let idx_size = u64::from_be_bytes(
+                        footer_bytes[FOOTER_SIZE / 2..FOOTER_SIZE]
+                            .try_into()
+                            .unwrap(),
+                    );
+
+                    let mut idx: Vec<u8> = vec![0; idx_size.try_into().unwrap()];
+                    // Parse the sparse index (offset back from FOOTER_SIZE)
+                    let n = seg_file.seek_read(&mut idx, idx_offset).unwrap();
+                    assert_eq!(idx_size, n.try_into().unwrap());
+
+                    let mut sparse_idx: BTreeMap<String, u64> = BTreeMap::new();
+                    let mut idx_cursor = &idx[..];
+
+                    while !idx_cursor.is_empty() {
+                        // First key-offset
+                        // key_len is u32 (4)
+                        // key is a variable length byte slice
+                        // offset is u64 (8)
+                        let mut key_len_bytes: [u8; 4] = [0; 4];
+                        idx_cursor.read_exact(&mut key_len_bytes).unwrap();
+                        let key_len = u32::from_be_bytes(key_len_bytes);
+                        let mut key_bytes = vec![0; key_len as usize];
+                        idx_cursor.read_exact(&mut key_bytes).unwrap();
+                        let mut offset_bytes: [u8; 8] = [0; 8];
+                        idx_cursor.read_exact(&mut offset_bytes).unwrap();
+                        let offset = u64::from_be_bytes(offset_bytes);
+
+                        sparse_idx.insert(String::from_utf8(key_bytes).unwrap().into(), offset);
+                    }
+
+                    let mut key_offset: u64 = 0;
+                    for (k, v) in sparse_idx.iter() {
+                        match k.as_str().cmp(cmd_seq[1]) {
+                            std::cmp::Ordering::Greater => break,
+                            std::cmp::Ordering::Equal => {
+                                key_offset = *v;
+                                break;
+                            }
+                            std::cmp::Ordering::Less => {
+                                key_offset = *v;
+                            }
+                        }
+                    }
+                    // Read until the end of the data block (either a zero byte or FOOTER_OFFET begins?)
+                    // Assume for now that the key exists actually!
+                    seg_file.seek(std::io::SeekFrom::Start(key_offset)).unwrap();
+
+                    loop {
+                        let current_position = seg_file.stream_position().unwrap();
+                        if current_position >= idx_offset {
+                            println!("Key not found");
+                            break;
+                        }
+
+                        let mut key_len_bytes: [u8; 4] = [0; 4];
+                        seg_file.read_exact(&mut key_len_bytes).unwrap();
+                        let key_len = u32::from_be_bytes(key_len_bytes);
+
+                        let mut key_bytes = vec![0; key_len as usize];
+                        seg_file.read_exact(&mut key_bytes).unwrap();
+
+                        let mut value_len_bytes: [u8; 4] = [0; 4];
+                        seg_file.read_exact(&mut value_len_bytes).unwrap();
+
+                        let value_len = u32::from_be_bytes(value_len_bytes);
+                        let mut value_bytes = vec![0; value_len as usize];
+                        seg_file.read_exact(&mut value_bytes).unwrap();
+
+                        // Assuming just String keys and values for now!
+                        let key = String::from_utf8(key_bytes).unwrap();
+                        let value = String::from_utf8(value_bytes).unwrap();
+
+                        if key.as_str() == cmd_seq[1] {
+                            println!("Found pair: {key}: {value}");
+                            break;
+                        }
+                        if key.as_str() > cmd_seq[1] {
+                            println!("Key not found");
+                            break;
+                        }
+                    }
+                    // If not, decrement the curr_seq and try again (assuming no "missing" segments)
+                    // This can be part of a higher order loop -> determine once you clean up?
                 }
             }
             _ => {
