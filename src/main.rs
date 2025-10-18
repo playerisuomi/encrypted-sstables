19b37e37fcd54215c90a947e46037f350c4e3a2b
kerosiinikone <onni.ahopelto@gmail.com>
2025-10-18 14:34:51 +0300

    Refactored KvStore state

--- src/main.rs
+++ src/main.rs
@@ -1,214 +1,33 @@
 use anyhow::Result;
-use enc_kv_store::encryption::DefaultEncrypter;
-use enc_kv_store::encryption::Encrypter;
-use enc_kv_store::segment::SegmentIter;
 use enc_kv_store::store::{KvError, KvStore};
-use enc_kv_store::{FOOTER_SIZE, MAX_MEMTABLE};
 use once_cell::sync::Lazy;
 use std::path::Path;
+use std::sync::Arc;
+use std::thread;
 use std::{
-    collections::BTreeMap,
     env::{self},
-    fs::{self, File, OpenOptions},
-    io::{Write, stdin},
-    ops::{Add, DerefMut},
-    sync::{Arc, Mutex, mpsc},
-    thread::{self},
-    vec,
+    fs::{self},
 };
 
 static DEFAULT: Lazy<String> = Lazy::new(|| String::from("password"));
 
-#[allow(unused_assignments)]
 fn main() -> Result<(), KvError> {
     let args: Vec<String> = env::args().collect();
-
     let password = args.get(1).unwrap_or(&DEFAULT);
     let curr_dir = env::current_dir().expect("curr dir");
 
-    let encrypter = DefaultEncrypter::new(password.to_owned()).expect("no default encryption");
-    // Could also just be a ref to the "main" encrypter instance -> figure out when modularizing the app loop
-    let mut hm: KvStore<String> = KvStore::new(encrypter.clone());
-    let (tx, rx) = mpsc::channel::<BTreeMap<String, String>>();
-
-    let encypter_guard = Arc::new(Mutex::new(encrypter));
-
-    if let Ok(file) = File::open(curr_dir.join("wal.log")) {
-        hm.sync_wal(file, password.clone()).unwrap();
-    }
     let latest_segment = get_dir_segment_count(env::current_dir().expect("curr dir").as_path())
         .expect("segment count");
 
-    let seg_num: Arc<Mutex<usize>> = Arc::new(Mutex::new(latest_segment));
-    let log_handle = Arc::new(Mutex::new(
-        OpenOptions::new()
-            .read(true)
-            .write(true)
-            .append(true)
-            .create(true)
-            .open(curr_dir.join(format!("wal.log")).as_path())
-            .expect("New log file error"),
+    let hm: Arc<KvStore<String>> = Arc::new(KvStore::new(
+        password.to_owned(),
+        latest_segment,
+        curr_dir.clone(),
     ));
 
-    let curr_dir_bg = curr_dir.clone();
-    let log_handle_bg = log_handle.clone();
-    let seg_bg = seg_num.clone();
-    let encrypter_bg = encypter_guard.clone();
-
-    let _ = thread::spawn(move || {
-        for flush_table in rx {
-            let mut seg_num = seg_bg.lock().unwrap();
-            let mut seg_handle = OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(
-                    curr_dir_bg
-                        .join(format!("segment_{}.sstable", seg_num))
-                        .as_path(),
-                )
-                .expect("New log file error");
-
-            let mut idx = Vec::new();
-            let mut buf = Vec::new();
-
-            let encrypter = encrypter_bg.lock().unwrap();
-
-            for (i, (k, v)) in flush_table.iter().enumerate() {
-                let offset = buf.len() as u64;
-
-                // Note: manually set V -> String (KvStore)
-                let value: String = v.parse().expect("invalid value");
-                let mut sealed_value: Vec<u8> =
-                    bincode::encode_to_vec(value, bincode::config::standard()).expect("encoding");
-
-                let n = encrypter
-                    .encrypt(&mut sealed_value, Some(k.as_bytes()))
-                    .expect("encrypting a value-pair");
-
-                let key_len: u32 = k.as_bytes().len() as u32;
-                let cipher_len: u32 = sealed_value.len() as u32;
-
-                buf.extend_from_slice(&key_len.to_be_bytes());
-                buf.extend_from_slice(k.as_bytes());
-                // Nonce (12)
-                buf.extend_from_slice(&n);
-                // Cipher (..)
-                buf.extend_from_slice(&cipher_len.to_be_bytes());
-                buf.extend_from_slice(sealed_value.as_slice());
-
-                if i % (flush_table.len() / 2) == 0 {
-                    idx.extend_from_slice(&key_len.to_be_bytes());
-                    idx.extend_from_slice(k.as_bytes());
-                    idx.extend_from_slice(&offset.to_be_bytes());
-                }
-            }
-            let mut footer: Vec<u8> = vec![0; FOOTER_SIZE];
-            footer[0..8].copy_from_slice(&buf.len().to_be_bytes());
-            footer[8..16].copy_from_slice(&idx.len().to_be_bytes());
-
-            let mut salt_bytes: [u8; 16] = [0u8; 16];
-            encrypter
-                .decode_salt_bytes(&mut salt_bytes)
-                .expect("salt decode");
-
-            footer[16..].copy_from_slice(&salt_bytes);
-
-            buf.extend(&idx);
-            buf.extend_from_slice(&footer);
-
-            seg_handle
-                .write_all(buf.as_slice())
-                .expect("unable to write");
-            *seg_num = seg_num.add(1);
-
-            rotate_log_file(
-                &&log_handle_bg,
-                &curr_dir_bg.join("wal.log").as_path(),
-                &curr_dir_bg.join("archive").as_path(),
-            )
-            .expect("rotate log")
-
-            // Note: Merge segments...
-        }
-    });
-
-    let lines = stdin().lines();
-    for line in lines {
-        let line = line.unwrap();
-        let cmd_seq: Vec<_> = line.split_whitespace().collect();
-
-        match cmd_seq[0] {
-            "SET" => {
-                assert!(cmd_seq[1..].len() == 2);
-                let (k, v) = (cmd_seq[1], cmd_seq[2]);
-                let (key_len, value_len) = (k.len(), v.len());
-
-                assert!(key_len <= u8::MAX as usize);
-                assert!(value_len <= u8::MAX as usize);
-
-                // V is manually set to String
-                hm.write_wal(
-                    k.to_string(),
-                    v.to_string(),
-                    log_handle.lock().unwrap().deref_mut(),
-                )
-                .expect("write wal");
-                hm.insert_parsed(k.to_string(), v.to_string())?;
-
-                let tx = tx.clone();
-                if hm.memtable.len() >= MAX_MEMTABLE {
-                    let flush_table: BTreeMap<String, _> = std::mem::take(&mut hm.memtable);
-                    tx.send(flush_table).unwrap();
-                }
-
-                println!("SET done")
-            }
-            "GET" => {
-                assert!(cmd_seq[1..].len() == 1);
-                if let Some(val) = hm.memtable.get(cmd_seq[1]) {
-                    println!("GET -> {}", val.clone())
-                } else {
-                    let curr_seg = seg_num.lock().unwrap().clone();
-
-                    let seg_iter = SegmentIter::new(
-                        (0..curr_seg).into_iter().collect(),
-                        curr_dir.clone(),
-                        password.to_owned(),
-                    );
-                    if let Ok(Some(value)) = seg_iter.find_key_in_segments(cmd_seq[1]) {
-                        println!("GET -> {}", value)
-                    } else {
-                        println!("Not found")
-                    }
-                }
-            }
-            _ => return Err(KvError("Unknown cmd")),
-        }
-    }
-    Ok(())
-}
-
-fn rotate_log_file(
-    log_handle: &Arc<Mutex<File>>,
-    log_path: &Path,
-    archive_dir: &Path,
-) -> Result<()> {
-    let mut log_guard = log_handle.lock().unwrap();
-    fs::create_dir_all(archive_dir)?;
-
-    let archive_path = archive_dir.join("wal_log");
-    if log_path.exists() {
-        fs::rename(log_path, &archive_path)?;
-    }
-
-    let new_log_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(true)
-        .open(log_path)?;
-
-    *log_guard = new_log_file;
-    Ok(())
+    let bg_hm = Arc::clone(&hm);
+    thread::spawn(move || bg_hm.run_bg_thread());
+    hm.run()
 }
 
 fn get_dir_segment_count(dir_path: &Path) -> Result<usize> {
@@ -226,34 +45,34 @@ fn get_dir_segment_count(dir_path: &Path) -> Result<usize> {
         .count())
 }
 
-#[cfg(test)]
-mod tests {
-    use enc_kv_store::encryption::DefaultEncrypter;
-    use enc_kv_store::store::KvStore;
-    use rand::Rng;
-    use rand::distr::{Alphanumeric, Uniform};
+// #[cfg(test)]
+// mod tests {
+//     use enc_kv_store::encryption::DefaultEncrypter;
+//     use enc_kv_store::store::KvStore;
+//     use rand::Rng;
+//     use rand::distr::{Alphanumeric, Uniform};
 
-    #[test]
-    fn test_random_str_type_once() {
-        let mut hm = KvStore::new(DefaultEncrypter::new(String::new()).unwrap());
-        // Custom function for "randoms" in KvStore?
-        let (k, v): (String, usize) = (
-            rand::rng()
-                .sample_iter(&Alphanumeric)
-                .take(1000)
-                .map(char::from)
-                .collect(),
-            rand::rng().sample(Uniform::new(10usize, 15).unwrap()),
-        );
-        hm.memtable.insert(k.clone(), v);
-        assert_eq!(&v, hm.memtable.get(&k).unwrap());
-    }
+//     #[test]
+//     fn test_random_str_type_once() {
+//         let mut hm = KvStore::new(DefaultEncrypter::new(String::new()).unwrap());
+//         // Custom function for "randoms" in KvStore?
+//         let (k, v): (String, usize) = (
+//             rand::rng()
+//                 .sample_iter(&Alphanumeric)
+//                 .take(1000)
+//                 .map(char::from)
+//                 .collect(),
+//             rand::rng().sample(Uniform::new(10usize, 15).unwrap()),
+//         );
+//         hm.memtable.insert(k.clone(), v);
+//         assert_eq!(&v, hm.memtable.get(&k).unwrap());
+//     }
 
-    // #[test]
-    // fn test_random_str_store_and_retrieve() {
-    //     let hm: KvStore<String> = KvStore::new();
-    // }
+//     #[test]
+//     fn test_random_str_store_and_retrieve() {
+//         let hm: KvStore<String> = KvStore::new();
+//     }
 
-    // Concurrency, on-disk saving, etc. to find edge cases?
-    // WALs
-}
+//     // Concurrency, on-disk saving, etc. to find edge cases?
+//     // WALs
+// }
--- src/store.rs
+++ src/store.rs
@@ -1,20 +1,322 @@
+use crate::FOOTER_SIZE;
+use crate::MAX_MEMTABLE;
 use crate::encryption::DecryptError;
 use crate::encryption::Decrypter;
 use crate::encryption::DefaultDecrypter;
 use crate::encryption::DefaultEncrypter;
 use crate::encryption::EncryptError;
 use crate::encryption::Encrypter;
+use crate::segment::SegmentIter;
+use anyhow::Result;
 use base64::Engine;
 use base64::prelude::BASE64_STANDARD;
 use core::fmt;
+use std::fmt::Debug;
+use std::fs;
+use std::fs::OpenOptions;
 use std::io::Read;
 use std::io::Write;
+use std::io::stdin;
+use std::ops::Add;
+use std::path::Path;
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
 use std::{
     collections::BTreeMap,
     fmt::{Display, Formatter},
     fs::File,
     str::FromStr,
 };
+
+#[derive(Debug)]
+pub struct KvStore<V> {
+    pub memtable: Arc<Mutex<BTreeMap<String, V>>>,
+    encypter_guard: Arc<Mutex<DefaultEncrypter>>,
+    seq_num: Arc<Mutex<usize>>,
+    log_handle: Arc<Mutex<File>>,
+
+    password: String,
+    curr_dir: PathBuf,
+
+    flush_tx: Sender<BTreeMap<String, V>>,
+    flush_rx: Arc<Mutex<Receiver<BTreeMap<String, V>>>>,
+}
+
+impl<V> KvStore<V>
+where
+    V: bincode::Decode<()> + FromStr + bincode::Encode + Send + Sync + Display,
+    <V as FromStr>::Err: Debug,
+{
+    pub fn new(password: String, latest_segment: usize, curr_dir: PathBuf) -> Self {
+        let encrypter = DefaultEncrypter::new(password.to_owned()).expect("no default encryption");
+        let (tx, rx) = mpsc::channel::<BTreeMap<String, V>>();
+        Self {
+            log_handle: Arc::new(Mutex::new(
+                OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .append(true)
+                    .create(true)
+                    .open(curr_dir.join(format!("wal.log")).as_path())
+                    .expect("New log file error"),
+            )),
+            seq_num: Arc::new(Mutex::new(latest_segment)),
+            memtable: Arc::new(Mutex::new(BTreeMap::new())),
+            flush_rx: Arc::new(Mutex::new(rx)),
+            encypter_guard: Arc::new(Mutex::new(encrypter)),
+            password: password,
+            curr_dir: curr_dir,
+            flush_tx: tx,
+        }
+    }
+
+    pub fn run(&self) -> Result<(), KvError> {
+        if let Ok(file) = File::open(self.curr_dir.join("wal.log")) {
+            self.sync_wal(file)?
+        }
+
+        let lines = stdin().lines();
+        for line in lines {
+            let line = line.unwrap();
+            let cmd_seq: Vec<_> = line.split_whitespace().collect();
+
+            match cmd_seq[0] {
+                "SET" => {
+                    assert!(cmd_seq[1..].len() == 2);
+                    let (k, v) = (cmd_seq[1], cmd_seq[2]);
+                    let (key_len, value_len) = (k.len(), v.len());
+
+                    assert!(key_len <= u8::MAX as usize);
+                    assert!(value_len <= u8::MAX as usize);
+
+                    self.write_wal(k.to_string(), v.parse().expect("invalid value"))
+                        .expect("write wal");
+                    self.insert_parsed(k.to_string(), v.to_string())?;
+
+                    let tx: mpsc::Sender<BTreeMap<String, V>> = self.flush_tx.clone();
+                    let mut memtable = self.memtable.lock().expect("get mut");
+                    if memtable.len() >= MAX_MEMTABLE {
+                        let flush_table: BTreeMap<String, _> = std::mem::take(&mut memtable);
+                        tx.send(flush_table).unwrap();
+                    }
+
+                    println!("SET done")
+                }
+                "GET" => {
+                    assert!(cmd_seq[1..].len() == 1);
+                    if let Some(val) = self.memtable.lock().expect("get lock").get(cmd_seq[1]) {
+                        println!("GET -> {}", val)
+                    } else {
+                        let curr_seg = self.seq_num.lock().unwrap().clone();
+
+                        let seg_iter = SegmentIter::new(
+                            (0..curr_seg).into_iter().collect(),
+                            self.curr_dir.clone(),
+                            self.password.to_owned(),
+                        );
+                        if let Ok(Some(value)) = seg_iter.find_key_in_segments(cmd_seq[1]) {
+                            println!("GET -> {}", value)
+                        } else {
+                            println!("Not found")
+                        }
+                    }
+                }
+                _ => return Err(KvError("Unknown cmd")),
+            }
+        }
+        Ok(())
+    }
+
+    pub fn run_bg_thread(&self) {
+        let log_handle_bg = self.log_handle.clone();
+        let seq_bg = self.seq_num.clone();
+        let encrypter_bg = self.encypter_guard.clone();
+
+        for flush_table in self.flush_rx.lock().expect("rx lock").iter() {
+            let mut seq_num = seq_bg.lock().unwrap();
+            let mut seq_handle = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(
+                    self.curr_dir
+                        .join(format!("segment_{}.sstable", seq_num))
+                        .as_path(),
+                )
+                .expect("New log file error");
+
+            let mut idx = Vec::new();
+            let mut buf = Vec::new();
+
+            for (i, (k, v)) in flush_table.iter().enumerate() {
+                let offset = buf.len() as u64;
+                let (sealed_bytes, nonce) =
+                    self.build_entry(&k, &v).expect("unable to build entry");
+
+                let key_len: u32 = k.as_bytes().len() as u32;
+                let cipher_len: u32 = sealed_bytes.len() as u32;
+
+                buf.extend_from_slice(&key_len.to_be_bytes());
+                buf.extend_from_slice(k.as_bytes());
+                buf.extend_from_slice(&nonce);
+                buf.extend_from_slice(&cipher_len.to_be_bytes());
+                buf.extend_from_slice(sealed_bytes.as_slice());
+
+                if i % (flush_table.len() / 2) == 0 {
+                    idx.extend_from_slice(&key_len.to_be_bytes());
+                    idx.extend_from_slice(k.as_bytes());
+                    idx.extend_from_slice(&offset.to_be_bytes());
+                }
+            }
+            let mut footer: Vec<u8> = vec![0; FOOTER_SIZE];
+            footer[0..8].copy_from_slice(&buf.len().to_be_bytes());
+            footer[8..16].copy_from_slice(&idx.len().to_be_bytes());
+
+            let mut salt_bytes: [u8; 16] = [0u8; 16];
+            encrypter_bg
+                .lock()
+                .expect("encrypter lock")
+                .decode_salt_bytes(&mut salt_bytes)
+                .expect("salt decode");
+
+            footer[16..].copy_from_slice(&salt_bytes);
+
+            buf.extend(&idx);
+            buf.extend_from_slice(&footer);
+
+            seq_handle
+                .write_all(buf.as_slice())
+                .expect("unable to write");
+            *seq_num = seq_num.add(1);
+
+            rotate_log_file(
+                &&log_handle_bg,
+                &self.curr_dir.join("wal.log").as_path(),
+                &self.curr_dir.join("archive").as_path(),
+            )
+            .expect("rotate log")
+
+            // Note: Merge segments...
+        }
+    }
+
+    pub fn sync_wal(&self, mut file: File) -> Result<(), KvError> {
+        let mut buf = String::new();
+        file.read_to_string(&mut buf).unwrap();
+
+        for entry in buf.lines() {
+            let cmd_seq: Vec<_> = entry.split_whitespace().collect();
+            match cmd_seq[0] {
+                "SET" => {
+                    assert!(cmd_seq.len() == 5);
+                    let mut key: Vec<u8> = Vec::from(cmd_seq[1].as_bytes());
+                    let mut enc_string: Vec<u8> =
+                        BASE64_STANDARD.decode(cmd_seq[2]).expect("decode value");
+                    let nonce: Vec<u8> = BASE64_STANDARD.decode(cmd_seq[3]).expect("decode nonce");
+                    let salt_bytes: Vec<u8> =
+                        BASE64_STANDARD.decode(cmd_seq[4]).expect("decode nonce");
+
+                    let enc_bytes = enc_string.as_mut_slice();
+                    let nonce_bytes: [u8; 12] = nonce.try_into().expect("invalid nonce bytes");
+                    let salt = DefaultDecrypter::encode_salt_string(salt_bytes.as_slice())
+                        .expect("salt string conversion");
+
+                    let log_decrypter = DefaultDecrypter::new(self.password.clone(), salt);
+
+                    if let Ok(plaintext_bytes) =
+                        log_decrypter.decrypt(enc_bytes, nonce_bytes, &mut key)
+                    {
+                        let (plain, _) = bincode::decode_from_slice(
+                            plaintext_bytes,
+                            bincode::config::standard(),
+                        )?;
+                        self.memtable
+                            .lock()
+                            .expect("insert lock")
+                            .insert(cmd_seq[1].to_string(), plain);
+                    }
+                }
+                _ => return Err(KvError("unknown cmd")),
+            };
+        }
+        Ok(())
+    }
+
+    pub fn write_wal(&self, k: String, v: V) -> Result<(), KvError>
+    where
+        V: bincode::Encode,
+    {
+        let (mut sealed_bytes, mut nonce) = self.build_entry(&k, &v)?;
+
+        let encrypter = self.encypter_guard.lock().expect("enuable to aqcuire lock");
+        let mut salt_bytes: [u8; 16] = [0u8; 16];
+        encrypter
+            .decode_salt_bytes(&mut salt_bytes)
+            .expect("salt decode");
+
+        let encoded_string = BASE64_STANDARD.encode(&mut sealed_bytes);
+        let nonce = BASE64_STANDARD.encode(&mut nonce);
+        let salt_encoded = BASE64_STANDARD.encode(&mut salt_bytes);
+
+        let log_entry = format!("SET {} {} {} {}\n", k, encoded_string, nonce, salt_encoded);
+        let mut buf = Vec::from(log_entry.as_bytes());
+
+        self.log_handle
+            .lock()
+            .expect("unable to lock file")
+            .write_all(&mut buf)?;
+        Ok(())
+    }
+
+    pub fn insert_parsed(&self, key: String, value: String) -> Result<(), KvError> {
+        if let Ok(value) = value.parse::<V>() {
+            self.memtable
+                .lock()
+                .expect("insert lock")
+                .insert(key, value);
+        } else {
+            return Err(KvError("insert"));
+        }
+        Ok(())
+    }
+
+    fn build_entry(&self, key: &str, value: &V) -> Result<(Vec<u8>, [u8; 12]), KvError> {
+        let encrypter = self
+            .encypter_guard
+            .lock()
+            .expect("unable to acquire a lock");
+        let mut sealed_bytes = bincode::encode_to_vec(value, bincode::config::standard())?;
+        let nonce = encrypter.encrypt(&mut sealed_bytes, Some(key.as_bytes()))?;
+        Ok((sealed_bytes, nonce))
+    }
+}
+
+fn rotate_log_file(
+    log_handle: &Arc<Mutex<File>>,
+    log_path: &Path,
+    archive_dir: &Path,
+) -> Result<()> {
+    fs::create_dir_all(archive_dir)?;
+
+    let mut log_guard = log_handle.lock().unwrap();
+    let archive_path = archive_dir.join("wal_log");
+
+    if log_path.exists() {
+        fs::rename(log_path, &archive_path)?;
+    }
+    let new_log_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(log_path)?;
+
+    *log_guard = new_log_file;
+
+    Ok(())
+}
 
 #[derive(Debug, Clone)]
 pub struct KvError(pub &'static str);
@@ -53,104 +355,5 @@ impl From<bincode::error::EncodeError> for KvError {
 impl From<bincode::error::DecodeError> for KvError {
     fn from(_: bincode::error::DecodeError) -> Self {
         KvError("Failed to decode")
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct KvStore<V> {
-    pub memtable: BTreeMap<String, V>,
-    encrypter: DefaultEncrypter,
-}
-
-impl<V> KvStore<V>
-where
-    V: bincode::Decode<()> + FromStr,
-{
-    pub fn new(encrypter: DefaultEncrypter /* for now, just default */) -> Self {
-        Self {
-            encrypter,
-            memtable: BTreeMap::new(),
-        }
-    }
-
-    pub fn sync_wal(&mut self, mut file: File, password: String) -> Result<(), KvError>
-    where
-        V: bincode::Decode<()> + FromStr,
-    {
-        let mut buf = String::new();
-        file.read_to_string(&mut buf).unwrap();
-
-        for entry in buf.lines() {
-            let cmd_seq: Vec<_> = entry.split_whitespace().collect();
-            match cmd_seq[0] {
-                "SET" => {
-                    assert!(cmd_seq.len() == 5);
-                    let mut key: Vec<u8> = Vec::from(cmd_seq[1].as_bytes());
-                    let mut enc_string: Vec<u8> =
-                        BASE64_STANDARD.decode(cmd_seq[2]).expect("decode value");
-                    let nonce: Vec<u8> = BASE64_STANDARD.decode(cmd_seq[3]).expect("decode nonce");
-                    let salt_bytes: Vec<u8> =
-                        BASE64_STANDARD.decode(cmd_seq[4]).expect("decode nonce");
-
-                    let enc_bytes = enc_string.as_mut_slice();
-                    let nonce_bytes: [u8; 12] = nonce.try_into().expect("invalid nonce bytes");
-                    let salt = DefaultDecrypter::encode_salt_string(
-                        salt_bytes
-                            .as_slice()
-                            .try_into()
-                            .expect("invalid salt bytes"),
-                    )
-                    .expect("salt string conversion");
-
-                    let log_decrypter = DefaultDecrypter::new(password.clone(), salt);
-
-                    if let Ok(plaintext_bytes) =
-                        log_decrypter.decrypt(enc_bytes, nonce_bytes, &mut key)
-                    {
-                        let (plain, _) = bincode::decode_from_slice(
-                            plaintext_bytes,
-                            bincode::config::standard(),
-                        )?;
-                        self.memtable.insert(cmd_seq[1].to_string(), plain);
-                    }
-                }
-                _ => return Err(KvError("unknown cmd")),
-            };
-        }
-        Ok(())
-    }
-
-    pub fn write_wal(&self, k: String, v: V, file: &mut File) -> Result<(), KvError>
-    where
-        V: bincode::Encode,
-    {
-        let mut sealed_bytes = bincode::encode_to_vec(v, bincode::config::standard())?;
-        let mut nonce = self
-            .encrypter
-            .encrypt(&mut sealed_bytes, Some(k.as_bytes()))?;
-
-        let mut salt_bytes: [u8; 16] = [0u8; 16];
-        self.encrypter
-            .decode_salt_bytes(&mut salt_bytes)
-            .expect("salt decode");
-
-        let encoded_string = BASE64_STANDARD.encode(&mut sealed_bytes);
-        let nonce = BASE64_STANDARD.encode(&mut nonce);
-        let salt_encoded = BASE64_STANDARD.encode(&mut salt_bytes);
-
-        let log_entry = format!("SET {} {} {} {}\n", k, encoded_string, nonce, salt_encoded);
-        let mut buf = Vec::from(log_entry.as_bytes());
-
-        file.write_all(&mut buf)?;
-        Ok(())
-    }
-
-    pub fn insert_parsed(&mut self, key: String, value: String) -> Result<(), KvError> {
-        if let Ok(value) = value.parse::<V>() {
-            self.memtable.insert(key, value);
-        } else {
-            return Err(KvError("insert"));
-        }
-        Ok(())
     }
 }
